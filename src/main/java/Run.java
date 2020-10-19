7763376fcea61e7c0f1d53cfa5146dbda8461da7
Roberto <roberto.cuyar44@gmail.com>
2020-10-19 12:08:26 -0500

    maven-exercises completion

---
+++ src/main/java/Run.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
+
+
+public class Run {
+    public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Enter Something: ");
+        String response = input.nextLine();
+        System.out.printf("You Entered: %s\n", response);
+        if(StringUtils.isNumeric(response)) {
+            System.out.printf("%s is a number\n", response);
+        }else {
+            System.out.printf("%s is not a number\n", response);
+        }
+        System.out.printf("Flipped Case: %s\n",StringUtils.swapCase(response));
+        System.out.printf("Reversed: %s\n", StringUtils.reverse(response));
+        }
+
+
+
+}
